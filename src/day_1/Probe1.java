33fe981b70258c50d98bc60e369655400378c149
Fatihkesik <59777619+Fatihkesik@users.noreply.github.com>
2020-05-14 22:38:47 +0200

    Update Probe1.java

--- src/day_1/Probe1.java
+++ src/day_1/Probe1.java
@@ -5,5 +5,9 @@ public class Probe1 {
     public static void main(String[] args) {
 
         //ohne Probe kann man nicht lernen
+        
+        //wir streben nach der Beste
+
+        
     }
 }
